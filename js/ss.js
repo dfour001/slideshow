3588b74a35f68d45c4f2b016eea92148a37739f4
dfour001 <53315984+dfour001@users.noreply.github.com>
2020-06-19 11:48:46 -0400

    Add more animation and customization

--- js/ss.js
+++ js/ss.js
@@ -1,13 +1,14 @@
-function load_slideshow(url) {    
+function load_slideshow(url) {
     $.getJSON(url, function (r) {
         console.log(r);
         let slides = r.slides;
-        start_slideshow(slides);
+        let titlePage = r.titlePage;
+        start_slideshow(titlePage, slides);
     });
 }
 
-function start_slideshow(slides) {
-    let curSlide = 0; // Index of slide currently being displayed
+function start_slideshow(titlePage, slides) {
+    let curSlide = -1; // Index of slide currently being displayed
     let slideCount = slides.length; // Total number of slides
 
     let curText = 0; // Index of slide text currently being displayed
@@ -16,48 +17,91 @@ function start_slideshow(slides) {
 
     // Set event listeners
     $('#btnNext').on('click', function () {
-        curText++;
-
+        // If title slide is displayed (curSlide == -1), set curSlide to 0
+        // Else increment the curText variable.
+        if (curSlide == -1) {
+            next_slide();
+            return
+        } else {
+            curText++;
+        };
 
         if (curSlide < slideCount) {
             // If more text, change text, but not slide
             if (curText < curTextCount) {
-                console.log('More text to display');
+
                 $('#text').html(slides[curSlide].text[curText]);
             } else { // If no more text, change to next slide
                 if (curSlide + 1 < slideCount) {
-                    console.log('No more text.  Next slide');
-                    curSlide++;
-                    next_slide(curSlide);
+                    next_slide();
                 };
-
-
-
-
             };
 
             if (curSlide + 1 == slideCount && curText + 1 == curTextCount) {
                 $('#btnNext').hide();
             }
-            console.log('slide ' + (curSlide + 1) + ' of ' + slideCount);
-            console.log('text ' + (curText + 1) + ' of ' + curTextCount);
 
-        }
+        };
     });
 
     // Display first slide
-    $('#title').html(slides[curSlide].title);
-    $('#text').html(slides[curSlide].text[curText]);
+    $('#title').html(titlePage.title);
+    $('#text').html(titlePage.text);
+    $('#img').css("background-image", "url(" + titlePage.images[0].url + ")");
 
 
+    function next_slide() {
+        // Increment slide
+        curSlide++;
+
+        // Animate slide out
+        slide_out();
+
+        // Update slide data and animate slide in
+        setTimeout(function () {
+            update_slide_data(curSlide);
+        }, 250);
+
+
+        // Animate slide in
+        setTimeout(function () {
+            slide_in();
+        }, 300);
+    }
     // Load next slide
-
-    function next_slide(curSlide) {
+    function update_slide_data(curSlide) {
         curText = 0;
         curTextCount = slides[curSlide].text.length;
         $('#slideNum').html(curSlide + 1);
         $('#title').html(slides[curSlide].title);
         $('#text').html(slides[curSlide].text[curText]);
+        $('#img').css("background-image", "url(" + slides[curSlide].images[0].url + ")");
+
+        let slide = $('#display');
+        let img = $('#img');
+        slide.removeClass('slideOut');
+        img.removeClass('slideOut');
+        slide.addClass('slideIn');
+        img.addClass('slideIn');
+    }
+
+    // Slide animations
+    function slide_out() {
+        let slide = $('#display');
+        let img = $('#img');
+        slide.removeClass(['slideShow', 'slideIn']);
+        img.removeClass(['slideShow', 'slideIn']);
+        slide.addClass('slideOut');
+        img.addClass('slideOut');
+    }
+
+    function slide_in() {
+        slide = $('#display');
+        let img = $('#img');
+        slide.removeClass('slideIn');
+        img.removeClass('slideIn');
+        slide.addClass('slideShow');
+        img.addClass('slideShow');
     }
 }
 
